22b9e392d77d908f52b558381805791f77a490eb
21817202 <84193320+21817202@users.noreply.github.com>
2021-05-19 12:05:42 +0800

    Routes created for Page 1 and 2, Updated the app

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'zfgb6554vddfgsrg54351f'
 
-from .views import views
-from .forms import forms
+# ----------------------- routes
+# main page
+@app.route('/')
+def intro():
+    return render_template('intro.html')
 
-app.register_blueprint(views, url_prefix='/')
-app.register_blueprint(forms, url_prefix ='/')
+# history page
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+
     
 if __name__ == '__main__':
     app.run(debug=True)
