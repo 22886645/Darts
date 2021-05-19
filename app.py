14cc6617ff2b80cb10f0fdeff75623b246b8f799
21817202 <84193320+21817202@users.noreply.github.com>
2021-05-19 14:58:42 +0800

    building database and models

--- app.py
+++ app.py
@@ -7,8 +7,83 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager,UserMixin,login_user,current_user,logout_user,login_required
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = 'zfgb6554vddfgsrg54351f'
+app.config['SECRET_KEY'] = 'zfgb6554vddfgsrg54351f' #for csrf token
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dart.db'  #name of db
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'First log in'
+
+# this model is has no use
+class Semigame(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+    num = db.Column(db.Integer, nullable=True)
+    date = db.Column(db.DateTime, default=datetime.now)
+    def __repr__(self):
+        return f'SemiGame({self.id} - {self.User_id}- {self.num}- {self.date})'
+
+# storing the game results
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False) #creating a one to many relationship with foreign key
+    score = db.Column(db.Integer, nullable=True)
+    date = db.Column(db.DateTime, default=datetime.now)
+    def __repr__(self):
+        return f'Game({self.id} - {self.User_id} - {self.score} - {self.date})' 
+
+# storing MCQ results 
+class Queeze(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    User_idhe user details
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.now)
+    queezes = db.relationship('Queeze',backref='player',lazy=True) # setting up a one to many relationship 
+    games = db.relationship('Game',backref='player',lazy=True)  # setting up a one to many relationship 
+    semigames = db.relationship('Semigame',backref='player',lazy=True)
+    def __repr__(self):
+        return f'User({self.id} - {self.username} - {self.email} - {self.password} - {self.date})'
+        # return '<User %r>' % self.username    
+
+
+
+# user page 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+ = db.Column(db.Integer,db.ForeignKey('user.id') ,nullable=False) #creating a one to many relationship with foreign key
+    score = db.Column(db.Integer, nullable=True)
+    date = db.Column(db.DateTime, default=datetime.now)
+    def __repr__(self):
+        return f'Queeze({self.id} - {self.User_id} - {self.score} - {self.date})'
+
+# storing the user details
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.now)
+    queezes = db.relationship('Queeze',backref='player',lazy=True) # setting up a one to many relationship 
+    games = db.relationship('Game',backref='player',lazy=True)  # setting up a one to many relationship 
+    semigames = db.relationship('Semigame',backref='player',lazy=True)
+    def __repr__(self):
+        return f'User({self.id} - {self.username} - {self.email} - {self.password} - {self.date})'
+        # return '<User %r>' % self.username    
+
+
+
+# user page 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 # ----------------------- routes
 # main page
